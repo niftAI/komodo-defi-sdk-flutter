8be9543f0b9832fd162ee0918e62dcbc1351007c
CharlVS <77973576+CharlVS@users.noreply.github.com>
2026-03-21 14:29:26 +0100

    debug(rpc): log orderbook responses

--- packages/komodo_defi_rpc_methods/lib/src/rpc_methods/orderbook/orderbook_rpc_namespace.dart
+++ packages/komodo_defi_rpc_methods/lib/src/rpc_methods/orderbook/orderbook_rpc_namespace.dart
@@ -73,7 +73,15 @@ class OrderbookMethodsNamespace extends BaseRpcMethodNamespace {
         base: base,
         rel: rel,
       ),
-    );
+    ).then((value) {
+      print(
+        '=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-='
+        'ORDERBOOK RESPONSE'
+        '${value.toJson()}'
+        '=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=',
+      );
+      return value;
+    });
   }
 
   /// Retrieves orderbook depth for multiple trading pairs.
